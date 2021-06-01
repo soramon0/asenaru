084afd2a22b80d1cda52630cc5ca388474387d5e
KarimLa <karimacc001@gmail.com>
2021-06-01 15:45:00 +0100

    formatted code

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,22 @@
-const { resolve } = require('path');
+const { resolve } = require('path')
 
 module.exports = {
-	roots: ['<rootDir>'],
-	testEnvironment: 'jest-environment-jsdom',
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
-	testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
-	transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
-	transform: {
-		'^.+\\.(ts|tsx)$': 'babel-jest',
-	},
-	watchPlugins: [
-		'jest-watch-typeahead/filename',
-		'jest-watch-typeahead/testname',
-	],
-	moduleNameMapper: {
-		'\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-		'\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
-		"@/components/(.*)$": resolve(__dirname, "./components/$1"),
-		"@/lib/(.*)$": resolve(__dirname, "./lib/$1")
-	},
+  roots: ['<rootDir>'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+    '@/components/(.*)$': resolve(__dirname, './components/$1'),
+    '@/lib/(.*)$': resolve(__dirname, './lib/$1'),
+  },
 }
